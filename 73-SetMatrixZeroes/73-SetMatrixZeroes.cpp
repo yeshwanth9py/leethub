93632721667fe4660969db1dcd07e28ccd43727d
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:37:22 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -0,0 +1,27 @@
+// Last updated: 6/11/2026, 11:37:15 AM
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> row(n, 1);
+        vector<int> col(m, 1);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 0;
+                    col[j] = 0;
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(row[i] == 0 || col[j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+    }
+};
