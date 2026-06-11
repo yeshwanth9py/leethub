3066e9a0f42a2b6d68c6ff1570930942050385d9
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:15:08 +0530

    Create 3872-FindMostFrequentVowelAndConsonant

---
+++ 3872-FindMostFrequentVowelAndConsonant/3872-FindMostFrequentVowelAndConsonant.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/11/2026, 11:15:00 AM
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        int freq[26], maxVowel = 0, maxConso = 0;
+        for (char c : s) {
+            int i = c - 'a';
+            freq[i]++;
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                maxVowel = max(maxVowel, freq[i]);
+            }
+            else {
+                maxConso = max(maxConso, freq[i]);
+            }
+        }
+        return maxVowel + maxConso;
+    }
+};
