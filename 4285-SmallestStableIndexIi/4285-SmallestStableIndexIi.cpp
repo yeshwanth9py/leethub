f7120ddaa90c1e4ced85bf4b6c25db566c7ae53a
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:09:33 +0530

    Create 4285-SmallestStableIndexIi

---
+++ 4285-SmallestStableIndexIi/4285-SmallestStableIndexIi.cpp
@@ -0,0 +1,26 @@
+// Last updated: 6/11/2026, 11:09:26 AM
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> maxtn(n, -1e9);
+        maxtn[0] = nums[0];
+        for(int i=1; i<n; i++){
+            maxtn[i] = max(maxtn[i-1], nums[i]);
+        }
+        vector<int> mintn(n, 1e9);
+        mintn[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            mintn[i] = min(mintn[i+1], nums[i]);
+        }
+
+        for(int i=0; i<n; i++){
+            int cur = maxtn[i] - mintn[i];
+            if(cur<=k) return i;
+        }
+
+        return -1;
+
+        //my own one idiots
+    }
+};
