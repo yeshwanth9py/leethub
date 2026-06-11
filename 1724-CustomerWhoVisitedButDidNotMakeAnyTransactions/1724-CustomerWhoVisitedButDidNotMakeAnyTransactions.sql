26e5afbbfd06e133a14bc1e5cf19bca9b5581a17
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:26:55 +0530

    Create 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions

---
+++ 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions/1724-CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,8 @@
+-- Last updated: 6/11/2026, 11:26:47 AM
+
+select v.customer_id, COUNT(v.visit_id) as count_no_trans
+from Visits as v left join Transactions as t on v.visit_id = t.visit_id
+where t.transaction_id is null
+group by v.customer_id;
+
+
