df4c9204a1c49804d7947edb7c38974538172e33
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:20:01 +0530

    Create 3154-MaximumValueOfAnOrderedTripletI

---
+++ 3154-MaximumValueOfAnOrderedTripletI/3154-MaximumValueOfAnOrderedTripletI.py
@@ -0,0 +1,9 @@
+# Last updated: 6/11/2026, 11:19:53 AM
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(len(nums)-2):
+            for j in range(i+1,len(nums)-1):
+                for k in range(j+1,len(nums)):
+                    ans= max(ans,(nums[i]-nums[j])*nums[k])
+        return ans
