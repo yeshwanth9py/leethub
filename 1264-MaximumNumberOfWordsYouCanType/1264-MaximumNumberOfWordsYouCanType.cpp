2ee88da11f20175ff75caa96d8d0902d1fe8eb0b
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:29:14 +0530

    Create 1264-MaximumNumberOfWordsYouCanType

---
+++ 1264-MaximumNumberOfWordsYouCanType/1264-MaximumNumberOfWordsYouCanType.cpp
@@ -0,0 +1,34 @@
+// Last updated: 6/11/2026, 11:29:07 AM
+class Solution {
+public:
+    int canBeTypedWords(string text, string broken){
+        unordered_set<char> brokenKeys;
+        int count = 0;
+
+        for (auto& c : broken){
+            brokenKeys.insert(c);
+        }
+
+        stringstream ss(text);
+        string word;
+        vector<string> words;
+
+        while (ss >> word){
+            words.push_back(word);
+        }
+
+        for (auto w : words)
+        {
+            for (char c : w)
+            {
+                if (brokenKeys.find(c) != brokenKeys.end())
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return words.size() - count;
+    }
+};
