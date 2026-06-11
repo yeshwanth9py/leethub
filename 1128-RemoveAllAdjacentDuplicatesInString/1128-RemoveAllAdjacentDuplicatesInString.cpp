8f64e84949e7ed08281677b821767c7e48cc13db
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:29:39 +0530

    Create 1128-RemoveAllAdjacentDuplicatesInString

---
+++ 1128-RemoveAllAdjacentDuplicatesInString/1128-RemoveAllAdjacentDuplicatesInString.cpp
@@ -0,0 +1,20 @@
+// Last updated: 6/11/2026, 11:29:31 AM
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        for(int i=0; i<s.length(); i++){
+            if(!st.empty() && st.top() == s[i]){
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans = st.top()+ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
