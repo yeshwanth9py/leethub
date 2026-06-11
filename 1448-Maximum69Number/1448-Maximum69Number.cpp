a7032198051e45f90f4ab2aa29d28f79ebf0c7e7
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:28:16 +0530

    Create 1448-Maximum69Number

---
+++ 1448-Maximum69Number/1448-Maximum69Number.cpp
@@ -0,0 +1,12 @@
+// Last updated: 6/11/2026, 11:28:08 AM
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int a=-1;
+        for(int n=num, d=0; n>0; n/=10, d++) {
+            int r=n%10;
+            if (r==6) a=d;
+        }
+        return (a==-1)?num:int(num+3*pow(10,a));     
+    }
+};
