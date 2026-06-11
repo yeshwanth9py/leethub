d5e506eb88b559aa9d25b21ba27a9b532e1f5b7c
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:26:01 +0530

    Create 1916-FindCenterOfStarGraph

---
+++ 1916-FindCenterOfStarGraph/1916-FindCenterOfStarGraph.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/11/2026, 11:25:53 AM
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int a = edges[0][0];
+        int b = edges[0][1];
+
+        int c = edges[1][0];
+        int d = edges[1][1];
+
+        if(a == c || a == d){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+};
