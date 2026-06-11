c51b716e0e58c8dc60d24708c2b84699ef5bb6a7
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:19:42 +0530

    Create 3172-DivisibleAndNonDivisibleSumsDifference

---
+++ 3172-DivisibleAndNonDivisibleSumsDifference/3172-DivisibleAndNonDivisibleSumsDifference.cpp
@@ -0,0 +1,12 @@
+// Last updated: 6/11/2026, 11:19:34 AM
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1 = 0, num2 = 0;
+        for (int i = 1 ; i <= n ; i++) {
+            if (i % m == 0) num2 += i;
+            else num1 += i;
+        }
+        return num1 - num2;
+    }
+};
