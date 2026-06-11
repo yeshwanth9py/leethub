dcaf756ad32101911a5d1ece0740bdbd59fc5846
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:26:59 +0530

    Create 1704-SpecialPositionsInABinaryMatrix

---
+++ 1704-SpecialPositionsInABinaryMatrix/1704-SpecialPositionsInABinaryMatrix.cpp
@@ -0,0 +1,31 @@
+// Last updated: 6/11/2026, 11:26:52 AM
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 1){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1){
+                    cnt++;
+                }   
+            }
+        }
+
+
+        return cnt;
+    }
+};
