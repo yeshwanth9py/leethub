8383856e0843b48f113e6f7fa0cb00d7d0634ab3
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:25:20 +0530

    Create 2048-BuildArrayFromPermutation

---
+++ 2048-BuildArrayFromPermutation/2048-BuildArrayFromPermutation.cpp
@@ -0,0 +1,12 @@
+// Last updated: 6/11/2026, 11:25:13 AM
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        for(int i=0; i<n; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+};
