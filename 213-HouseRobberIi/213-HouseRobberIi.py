9288a1fa2d957f12603d1d5d3fc44ff546fd309e
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:34:52 +0530

    Create 213-HouseRobberIi

---
+++ 213-HouseRobberIi/213-HouseRobberIi.py
@@ -0,0 +1,13 @@
+# Last updated: 6/11/2026, 11:34:45 AM
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def helper(nums):
+            rob1 = 0
+            rob2 = 0
+            temp = 0
+            for n in nums:
+                temp = max(rob1+n,rob2)
+                rob1 = rob2
+                rob2 = temp
+            return temp
+        return max(nums[0],helper(nums[1:]),helper(nums[:-1]))
