3c4ec0ecf2fe24eac9e6c2eb64e1f4729735b099
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:23:35 +0530

    Create 2277-CountEqualAndDivisiblePairsInAnArray

---
+++ 2277-CountEqualAndDivisiblePairsInAnArray/2277-CountEqualAndDivisiblePairsInAnArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/11/2026, 11:23:28 AM
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if((i*j)%k == 0 && (nums[i]==nums[j])){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
