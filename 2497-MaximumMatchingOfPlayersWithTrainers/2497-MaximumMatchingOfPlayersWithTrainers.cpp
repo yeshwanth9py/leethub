e59d1d0dbf704abba07498c165dde4ed5a4c7cc7
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:22:21 +0530

    Create 2497-MaximumMatchingOfPlayersWithTrainers

---
+++ 2497-MaximumMatchingOfPlayersWithTrainers/2497-MaximumMatchingOfPlayersWithTrainers.cpp
@@ -0,0 +1,24 @@
+// Last updated: 6/11/2026, 11:22:13 AM
+class Solution {
+public:
+    int matchPlayersAndTrainers(std::vector<int>& players, std::vector<int>& trainers) {
+        std::sort(players.begin(), players.end());
+        std::sort(trainers.begin(), trainers.end());
+
+        int count = 0;
+        int i = 0, j = 0; // pointers for players and trainers
+
+        while (i < players.size() && j < trainers.size()) {
+            if (players[i] <= trainers[j]) {
+                // Match found
+                count++;
+                i++;
+                j++;
+            } else {
+                // Try next trainer
+                j++;
+            }
+        }
+        return count;
+    }
+};
