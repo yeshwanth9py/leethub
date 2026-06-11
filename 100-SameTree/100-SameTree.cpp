6faf0774d374234997183cdab1ebe8425d8a3c02
yeshwanth sai barri <100275268+yeshwanth9py@users.noreply.github.com>
2026-06-11 11:36:53 +0530

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.cpp
@@ -0,0 +1,34 @@
+// Last updated: 6/11/2026, 11:36:45 AM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q==nullptr){
+            return true;
+        }
+
+        if(p == nullptr){
+            return false;
+        }
+
+        if(q == nullptr){
+            return false;
+        }
+
+        if(p->val == q->val){
+            return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+        }
+        return false;
+
+    }
+};
